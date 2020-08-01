6014ff07d2c181b5fa5468ad0d0d779aaade75ac
Weinber Krisztián <48327335+weinberkrisztian@users.noreply.github.com>
2020-08-01 13:10:06 +0200

    Update ProductCategoryRepository.java

--- src/main/java/eCommerceApp/dao/ProductCategoryRepository.java
+++ src/main/java/eCommerceApp/dao/ProductCategoryRepository.java
@@ -5,7 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin("http://hosting2078848.online.pro")
+//@CrossOrigin("http://hosting2078848.online.pro")
+@CrossOrigin("http://weinberwebshop.s3-website.eu-central-1.amazonaws.com")
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory,Long> {
 }
